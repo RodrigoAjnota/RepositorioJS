1d0500570442dd5efbfb82371611acb5771cb2ea
Rodrigo Ajnota <rodrigo.ajnota1@gmail.com>
2022-07-28 11:56:10 -0300

    añadi 3er desafío

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ if(marca === "audi" && (precio <= 20000 && precio >= 10000)){
 
 
 //Segundo Desafio
-function elegir(nombre, edad, genero){
+/*function elegir(nombre, edad, genero){
     console.log("El nombre de tu personaje es " + nombre)
     console.log("La edad de tu persoanje es " + edad)
     console.log("El genero de tu personaje es " + genero)
@@ -44,4 +44,42 @@ do{
     elegir(nombre, edad, genero)
     console.log("¡Personaje terminado!")
     seguir = prompt("Quieres continuar Y/N").toLowerCase()
-}while(seguir == "y")
+}while(seguir == "y")*/
+
+//Tercer Desafío
+class Jugador{
+    constructor(nombre, nivel, edad, genero){
+        this.nombre = nombre
+        this.nivel = nivel
+        this.edad = edad
+        this.genero = genero
+    }
+}
+
+const jugador1 = new Jugador("pepito", 20, 18, "M")
+const jugador2 = new Jugador("maria", 15, 19, "F")
+const jugador3 = new Jugador("juan", 17, 16, "M")
+
+const jugadores = [jugador1, jugador2, jugador3]
+
+let elegir, finalizar
+
+do{
+    alert("¡Bienvenido jugador! Selecione a su personaje:")
+    do{
+        elegir = prompt("Elige " + jugadores[0].nombre + ", " + jugadores[1].nombre + ", " + jugadores[2].nombre).toLowerCase()
+        if((elegir != jugadores[0].nombre) && (elegir != jugadores[1].nombre) && (elegir != jugadores[2].nombre)){
+            alert("Usuario incorrecto")
+        }
+    }while((elegir != jugadores[0].nombre) && (elegir != jugadores[1].nombre) && (elegir != jugadores[2].nombre))
+    finalizar = prompt("Quieres finalizar? Y/N").toLowerCase()
+    if(elegir === "pepito"){
+        console.table(jugador1)
+    } else if(elegir === "maria"){
+        console.table(jugador2)
+    } else{
+        console.table(jugador3)
+    }
+}while(finalizar == "n")
+
+
