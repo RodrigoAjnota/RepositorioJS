edb5c205be0d68cd448e34e128e14bee8928e9e3
Rodrigo Ajnota <rodrigo.ajnota1@gmail.com>
2022-08-15 19:56:05 -0300

    1er entregable terminado

--- script.js
+++ script.js
@@ -1,101 +1,90 @@
-//Primer Desafío
-/*let marca = prompt("Ingrese una marca").toLowerCase()
-let precio = parseFloat(prompt("Ingrese un precio"))
-
-if(marca === "audi" && (precio <= 20000 && precio >= 10000)){
-    console.log("Se añadio a tu carrito el Audi de $" + precio)
-} else if(marca === "toyota" && (precio <= 30000 && precio >= 20000)){
-    console.log("Se añadio a tu carrito el Toyota de $" + precio)
-} else if(marca === "nissan" && (precio <= 40000 && precio >= 30000)){
-    console.log("Se añadio a tu carrito el Nissan de $" + precio)
-}else{
-    alert("No tenemos ese auto a ese precio, lo sentimos.")
-}
-
-
-//Segundo Desafio
-function elegir(nombre, edad, genero){
-    console.log("El nombre de tu personaje es " + nombre)
-    console.log("La edad de tu persoanje es " + edad)
-    console.log("El genero de tu personaje es " + genero)
-}
-
-let nombre, edad, genero, seguir
-
-do{
-    do{
-        nombre =  prompt("Elija el nombre de su personaje").toLowerCase()
-        if(!(isNaN(nombre))){
-            alert("Nombre no valido")
-        }
-    }while(!(isNaN(nombre)))
-    do{
-        edad = parseFloat(prompt("Elija una edad para su personaje"))
-        if(isNaN(edad)){
-            alert("Edad no valida")
-        }
-    }while(isNaN(edad))
-    do{
-        genero = prompt("Elije entre M/F").toLowerCase()
-        if(genero != "m" && genero != "f"){
-            alert("Genero no valido")
-        }
-    }while(genero != "m" && genero != "f")
-    elegir(nombre, edad, genero)
-    console.log("¡Personaje terminado!")
-    seguir = prompt("Quieres continuar Y/N").toLowerCase()
-}while(seguir == "y")*/
-
-//Tercer Desafío
-class Jugador{
-    constructor(nombre, nivel, edad, genero){
-        this.nombre = nombre
-        this.nivel = nivel
-        this.edad = edad
-        this.genero = genero
-    }
-}
-
-const jugador1 = new Jugador("pepito", 20, 18, "M")
-const jugador2 = new Jugador("maria", 15, 19, "F")
-const jugador3 = new Jugador("juan", 17, 16, "M")
-
-const jugadores = []
-jugadores.push(jugador1,jugador2,jugador3)
-
-let elegir, finalizar
-
-do{
-    alert("¡Bienvenido jugador! Selecione a su personaje:")
-    do{
-        elegir = prompt("Elige " + jugadores[0].nombre + ", " + jugadores[1].nombre + ", " + jugadores[2].nombre).toLowerCase()
-        if((elegir != jugadores[0].nombre) && (elegir != jugadores[1].nombre) && (elegir != jugadores[2].nombre)){
-            alert("Usuario incorrecto")
-        }
-    }while((elegir != jugadores[0].nombre) && (elegir != jugadores[1].nombre) && (elegir != jugadores[2].nombre))
-    finalizar = prompt("Quieres finalizar? Y/N").toLowerCase()
-    if(elegir === "pepito"){
-        console.table(jugador1)
-    } else if(elegir === "maria"){
-        console.table(jugador2)
-    } else{
-        console.table(jugador3)
-    }
-}while(finalizar == "n")
-
-//1er Entrega del Proyecto Final
-/*class Concesionario{
-    constructor(marca, km, precio, modelo){
+//1er Entrega del Prosiecto Final
+class Concesionario{
+    constructor(marca, cv, precio, modelo){
         this.marca = marca
-        this.km = km
+        this.cv = cv
         this.precio = precio
         this.modelo = modelo
     }
 }
 
-const auto1 = new Concesionario ["toyota", 10, 23550, "corolla"]
-const auto2 = new Concesionario ["audi", 10, 21840, "a1"]
-const auto3 = new Concesionario ["nissan", 10, 23800, "juke"]
-const auto4 = new Concesionario ["bmw", 10, 44400, "serie 3"]
-const auto5 = new Concesionario ["ford", 10, 56731, "mustang"]*/
+const auto1 = new Concesionario ("toyota", 100, 36550, "corolla")
+const auto2 = new Concesionario ("audi", 100, 21840, "a1")
+const auto3 = new Concesionario ("nissan", 100, 29800, "juke")
+const auto4 = new Concesionario ("bmw", 100, 39400, "serie 3")
 
+const comprarAutos = [auto1, auto2, auto3, auto4]
+const autosComprados = []
+
+const listaMarcas = comprarAutos.map(autoMarcas => autoMarcas.marca)
+const listaPrecios = comprarAutos.map(autoPrecios => autoPrecios.precio)
+
+do{//Tienes que tener la console abierta antes de este paso
+    iniciar = prompt("¡Bienvenido a nuestra Concesionaria! Desea comprar un auto ? Si/No").toLowerCase()
+    if(iniciar == "si"){
+        alert("Aca podra ver todas nuestras marcas: " + listaMarcas)
+        alert("Y estos son los precios de cada auto: " + listaPrecios)
+    }else if(iniciar == "no"){
+        alert("Porfa diga que si :C")
+    }else{
+        alert("Eliga una de las opciones")
+    }
+}while(iniciar != "si")
+
+const autoPrecioMenor = comprarAutos.filter((filtos) => filtos.precio < 30000)
+const autoPrecioMayor = comprarAutos.filter((filtos) => filtos.precio > 30000)
+
+do{
+    comprar = parseFloat(prompt("Cuanto es su presupuesto?"))
+    if(comprar >= 20000 && comprar < 30000){ 
+        console.table(autoPrecioMenor) 
+        do{
+            comprarAu = prompt("Que auto desea comprar ?").toLowerCase()
+            switch (comprarAu){
+                case "audi" :
+                    alert("Auto comprado")
+                    autosComprados.push(auto2)
+                    break
+                case "nissan" :
+                    alert("Auto comprado")
+                    autosComprados.push(auto3)
+                    break
+                default : 
+                    alert("Ingrese una marca correcta")
+                    break
+            }
+            repetir = prompt("Desea comprar otro auto? Si/No").toLowerCase()
+        }while(repetir == "si")
+    } else if(comprar >= 30000 && comprar <= 40000){
+        console.table(autoPrecioMayor)
+        do{
+            comprarAu = prompt("Que auto desea comprar ?").toLowerCase()
+            switch (comprarAu){
+                case "toyota" :
+                    alert("Auto comprado")
+                    autosComprados.push(auto1)
+                    break
+                case "bmw" :
+                    alert("Auto comprado")
+                    autosComprados.push(auto4)
+                    break
+                default : 
+                    alert("Ingrese una marca correcta")
+                    break
+            }
+            repetir = prompt("Desea comprar otro auto? Si/No").toLowerCase()
+        }while(repetir == "si")
+    } else{
+        alert("No tenemos un auto a ese precio")
+    }
+    seguir = prompt("Desea seguir? Si/No").toLowerCase()
+}while(seguir == "si")
+
+function compraFinal(){
+    autosComprados.forEach((listaCompras) => {
+        console.table(listaCompras)
+    })
+}
+
+alert("Gracias por su compra y por elegirnos!")
+compraFinal()
